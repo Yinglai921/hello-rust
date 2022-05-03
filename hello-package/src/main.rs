2c3df18e72e1eaa30e8edfd703bca00e13772b2a
yinglai xu <yinglai.xu@nentgroup.com>
2022-05-03 14:02:54 +0200

    folder stucture in rust

---
+++ hello-package/src/main.rs
@@ -0,0 +1,10 @@
+/**
+ * example of folder structure in rust
+ * https://practice.rs/crate-module/intro.html
+ */
+mod front_of_house;
+
+fn main() {
+    assert_eq!(front_of_house::hosting::seat_at_table(), "sit down please");
+    assert_eq!(hello_package::eat_at_restaurant(), "yummy yummy!");
+}
